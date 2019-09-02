19e481dc362bff482b1e3e60d889e60313a0c627
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-09-02 19:28:28 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,98 +1,116 @@
-const express=require("express")
-const app=express()
-const fs=require('fs')
-const tours=JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-)
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
 
-app.use(express.json())
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
+const viewRouter = require('./routes/viewRoutes');
 
-app.get('/api/v1/tours',(req,res)=>{
-  res.status(200).json({
-    status:"success",
-    result:tours.length,
-    data:{
-      tours
-    }
-  })
-})
+// Start express app
+const app = express();
 
+app.enable('trust proxy');
 
-app.get('/api/v1/tours/:id',(req,res)=>{
-console.log(req.params);
-const id=req.params.id*1
-const tour=tours.find(x=>x.id===(id))
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
-if(!tour){
-return  res.status(404).json({
-  status:"fail",
-  message:"invalid"
-})
+// 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-  res.status(200).json({
-    status:"success",
-    result:tours.length,
-    data:{
-      tour
-    }
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post(
+  '/webhook-checkout',
+  bodyParser.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
   })
-})
+);
 
+app.use(compression());
 
-app.post('/api/v1/tours',(req,res)=>{
-  console.log(req.body);
-  const newId=tours[tours.length-1].id+1;
-  const newTour=Object.assign({id:newId},req.body);
-  res.send("done")
-tours.push(newTour)
-fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
-res.status(201).json({
-  status:'success',
-  data:{
-    tour:'newTour'
-  }
-})
-})
-})
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
 
+// 3) ROUTES
+app.use('/', viewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
-app.patch('/api/v1/tours/:id',(req,res)=>{
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-  if(req.params.id>tours.length){
-  return  res.status(404).json({
-    status:"fail",
-    message:"invalid"
-  })
-  }
+app.use(globalErrorHandler);
 
-  res.status(201).json({
-    status:'success',
-    data:{
-      tour:'Updated'
-    }
-  })
-})
-
-
-
-app.delete('/api/v1/tours/:id',(req,res)=>{
-
-  if(req.params.id*1>tours.length){
-  return  res.status(404).json({
-    status:"fail",
-    message:"invalid"
-  })
-  }
-
-  res.status(204).json({
-    status:'success',
-    data:null
-  })
-})
-
-//204:no content
-
-
-
-app.listen(3000,()=>console.log("server started!"))
+module.exports = app;
